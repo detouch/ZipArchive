9650b796c72f8acc6fedbd4e66a148c3545f2505
Daniel Estermann <dani@detouch.ch>
2014-10-03 18:40:19 +0200

    overwrite only older files == skip files with same or newer date, even with overwrite=YES

--- ZipArchive.m
+++ ZipArchive.m
@@ -263,8 +263,9 @@
 -(BOOL) UnzipFileTo:(NSString*) path overWrite:(BOOL) overwrite
 {
 	BOOL success = YES;
-    int index = 0;
-    int progress = -1;
+	int index = 0;
+	int skipped = 0;
+	int progress = -1;
 	int ret = unzGoToFirstFile( _unzFile );
 	unsigned char		buffer[4096] = {0};
 	if( ret!=UNZ_OK )
@@ -275,142 +276,176 @@
 	const char* password = [_password cStringUsingEncoding:NSASCIIStringEncoding];
 	
 	do{
-        @autoreleasepool {
-            if( [_password length]==0 )
-                ret = unzOpenCurrentFile( _unzFile );
-            else
-                ret = unzOpenCurrentFilePassword( _unzFile, password );
-            if( ret!=UNZ_OK )
-            {
-                [self OutputErrorMessage:@"Error occurs"];
-                success = NO;
-                break;
-            }
-            // reading data and write to file
-            int read ;
-            unz_file_info	fileInfo ={0};
-            ret = unzGetCurrentFileInfo(_unzFile, &fileInfo, NULL, 0, NULL, 0, NULL, 0);
-            if( ret!=UNZ_OK )
-            {
-                [self OutputErrorMessage:@"Error occurs while getting file info"];
-                success = NO;
-                unzCloseCurrentFile( _unzFile );
-                break;
-            }
-            char* filename = (char*) malloc( fileInfo.size_filename +1 );
-            unzGetCurrentFileInfo(_unzFile, &fileInfo, filename, fileInfo.size_filename + 1, NULL, 0, NULL, 0);
-            filename[fileInfo.size_filename] = '\0';
-            
-            // check if it contains directory
-            NSString * strPath = [NSString stringWithCString:filename encoding:self.stringEncoding];
-            BOOL isDirectory = NO;
-            if( filename[fileInfo.size_filename-1]=='/' || filename[fileInfo.size_filename-1]=='\\')
-                isDirectory = YES;
-            free( filename );
-            if( [strPath rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"/\\"]].location!=NSNotFound )
-            {// contains a path
-                strPath = [strPath stringByReplacingOccurrencesOfString:@"\\" withString:@"/"];
-            }
-            NSString* fullPath = [path stringByAppendingPathComponent:strPath];
-            
-            if( isDirectory )
-                [_fileManager createDirectoryAtPath:fullPath withIntermediateDirectories:YES attributes:nil error:nil];
-            else
-                [_fileManager createDirectoryAtPath:[fullPath stringByDeletingLastPathComponent] withIntermediateDirectories:YES attributes:nil error:nil];
-            
-            FILE* fp = NULL;
-            do
-            {
-                read = unzReadCurrentFile(_unzFile, buffer, 4096);
-                if (read >= 0)
-                {
-                    if (fp == NULL) {
-                        if( [_fileManager fileExistsAtPath:fullPath] && !isDirectory && !overwrite )
-                        {
-                            if( ![self OverWrite:fullPath] )
-                            {
-                                // don't process any more of the file, but continue
-                                break;
-                            }
-                        }
-                        if (!isDirectory) {
-                            fp = fopen( (const char*)[fullPath UTF8String], "wb");
-                            if (fp == NULL) {
-                                [self OutputErrorMessage:@"Failed to open output file for writing"];
-                                break;
-                            }
-                        }
-                    }
-                    fwrite(buffer, read, 1, fp );
-                }
-                else // if (read < 0)
-                {
-                    ret = read; // result will be an error code
-                    success = NO;
-                    [self OutputErrorMessage:@"Failed to read zip file"];
-                }
-            } while (read > 0);
-            
-            if (fp)
-            {
-                fclose( fp );
-                
-                // add the full path of this file to the output array
-                [(NSMutableArray*)_unzippedFiles addObject:fullPath];
-                
-                // set the orignal datetime property
-                if( fileInfo.tmu_date.tm_year!=0 )
-                {
-                    NSDateComponents* components = [[NSDateComponents alloc] init];
-                    components.second = fileInfo.tmu_date.tm_sec;
-                    components.minute = fileInfo.tmu_date.tm_min;
-                    components.hour = fileInfo.tmu_date.tm_hour;
-                    components.day = fileInfo.tmu_date.tm_mday;
-                    components.month = fileInfo.tmu_date.tm_mon;
-                    components.year = fileInfo.tmu_date.tm_year;
-                    
-                    NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-                    NSDate* orgDate = [[gregorianCalendar dateFromComponents:components] retain];
-                    [components release];
-                    [gregorianCalendar release];
-                    
-                    NSDictionary* attr = [NSDictionary dictionaryWithObject:orgDate forKey:NSFileModificationDate]; //[_fileManager fileAttributesAtPath:fullPath traverseLink:YES];
-                    if( attr )
-                    {
-                        //	[attr  setValue:orgDate forKey:NSFileCreationDate];
-                        if( ![_fileManager setAttributes:attr ofItemAtPath:fullPath error:nil] )
-                        {
-                            // cann't set attributes 
-                            NSLog(@"Failed to set attributes");
-                        }
-                        
-                    }
-                    [orgDate release];
-                    orgDate = nil;
-                }
-                
-            }
-            
-            if (ret == UNZ_OK) {
-                ret = unzCloseCurrentFile( _unzFile );
-                if (ret != UNZ_OK) {
-                    [self OutputErrorMessage:@"file was unzipped but failed crc check"];
-                    success = NO;
-                }
-            }
-            
-            if (ret == UNZ_OK) {
-                ret = unzGoToNextFile( _unzFile );
-            }
-            
-            if (_progressBlock && _numFiles) {
-                index++;
-                int p = index*100/_numFiles;
-                progress = p;
-                _progressBlock(progress, index, _numFiles);
-            }
-        }
+		@autoreleasepool {
+			if( [_password length]==0 )
+				ret = unzOpenCurrentFile( _unzFile );
+			else
+				ret = unzOpenCurrentFilePassword( _unzFile, password );
+			if( ret!=UNZ_OK )
+			{
+				[self OutputErrorMessage:@"Error occurs"];
+				success = NO;
+				break;
+			}
+			// reading data and write to file
+			int read ;
+			unz_file_info	fileInfo ={0};
+			ret = unzGetCurrentFileInfo(_unzFile, &fileInfo, NULL, 0, NULL, 0, NULL, 0);
+			if( ret!=UNZ_OK )
+			{
+				[self OutputErrorMessage:@"Error occurs while getting file info"];
+				success = NO;
+				unzCloseCurrentFile( _unzFile );
+				break;
+			}
+			char* filename = (char*) malloc( fileInfo.size_filename +1 );
+			unzGetCurrentFileInfo(_unzFile, &fileInfo, filename, fileInfo.size_filename + 1, NULL, 0, NULL, 0);
+			filename[fileInfo.size_filename] = '\0';
+			
+			// ============ BEGIN CHANGES TIGERLINE ==================
+			// set the orignal datetime property
+			NSDate* orgDate = nil;
+			
+			//{{ thanks to brad.eaton for the solution
+			NSDateComponents *dc = [[NSDateComponents alloc] init];
+			
+			dc.second = fileInfo.tmu_date.tm_sec;
+			dc.minute = fileInfo.tmu_date.tm_min;
+			dc.hour = fileInfo.tmu_date.tm_hour;
+			dc.day = fileInfo.tmu_date.tm_mday;
+			dc.month = fileInfo.tmu_date.tm_mon+1; // ZIP month is 0-based!
+			dc.year = fileInfo.tmu_date.tm_year;
+			
+			NSCalendar *gregorian = [[NSCalendar alloc]
+									 initWithCalendarIdentifier:NSGregorianCalendar];
+			
+			orgDate = [gregorian dateFromComponents:dc] ;
+			[dc release];
+			[gregorian release];
+			// ============ END CHANGES TIGERLINE ==================
+			
+			// check if it contains directory
+			NSString * strPath = [NSString stringWithCString:filename encoding:self.stringEncoding];
+			BOOL isDirectory = NO;
+			if( filename[fileInfo.size_filename-1]=='/' || filename[fileInfo.size_filename-1]=='\\')
+				isDirectory = YES;
+			free( filename );
+			if( [strPath rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"/\\"]].location!=NSNotFound )
+			{// contains a path
+				strPath = [strPath stringByReplacingOccurrencesOfString:@"\\" withString:@"/"];
+			}
+			NSString* fullPath = [path stringByAppendingPathComponent:strPath];
+			
+			if( isDirectory )
+				[_fileManager createDirectoryAtPath:fullPath withIntermediateDirectories:YES attributes:nil error:nil];
+			else
+				[_fileManager createDirectoryAtPath:[fullPath stringByDeletingLastPathComponent] withIntermediateDirectories:YES attributes:nil error:nil];
+			
+			FILE* fp = NULL;
+			do
+			{
+				read = unzReadCurrentFile(_unzFile, buffer, 4096);
+				if (read >= 0)
+				{
+					if (fp == NULL) {
+						if( [_fileManager fileExistsAtPath:fullPath] && !isDirectory && overwrite )
+						{
+							// ============ BEGIN CHANGES TIGERLINE ==================
+							NSDictionary* attributes = [_fileManager attributesOfItemAtPath:fullPath error:nil];
+							NSDate* fileDate = [attributes fileModificationDate];
+							BOOL extistingFileIsOlder = [fileDate timeIntervalSinceDate:orgDate] < 0;
+							if( !extistingFileIsOlder || ![self OverWrite:fullPath])
+							{
+								skipped++;
+								
+								// don't process any more of the file, but continue
+								break;
+							}
+							// ============ END CHANGES TIGERLINE ==================
+						}
+						if (!isDirectory) {
+							fp = fopen( (const char*)[fullPath UTF8String], "wb");
+							if (fp == NULL) {
+								[self OutputErrorMessage:@"Failed to open output file for writing"];
+								break;
+							}
+						}
+					}
+					fwrite(buffer, read, 1, fp );
+				}
+				else // if (read < 0)
+				{
+					ret = read; // result will be an error code
+					success = NO;
+					[self OutputErrorMessage:@"Failed to read zip file"];
+				}
+			} while (read > 0);
+			
+			if (fp)
+			{
+				fclose( fp );
+				
+				// add the full path of this file to the output array
+				[(NSMutableArray*)_unzippedFiles addObject:fullPath];
+				
+				// set the orignal datetime property
+				if( fileInfo.tmu_date.tm_year!=0 )
+				{
+					NSDateComponents* components = [[NSDateComponents alloc] init];
+					components.second = fileInfo.tmu_date.tm_sec;
+					components.minute = fileInfo.tmu_date.tm_min;
+					components.hour = fileInfo.tmu_date.tm_hour;
+					components.day = fileInfo.tmu_date.tm_mday;
+					components.month = fileInfo.tmu_date.tm_mon+1;
+					components.year = fileInfo.tmu_date.tm_year;
+					
+					NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+					NSDate* orgDate = [[gregorianCalendar dateFromComponents:components] retain];
+					[components release];
+					[gregorianCalendar release];
+					
+					NSDictionary* attr = [NSDictionary dictionaryWithObject:orgDate forKey:NSFileModificationDate]; //[_fileManager fileAttributesAtPath:fullPath traverseLink:YES];
+					if( attr )
+					{
+						//	[attr  setValue:orgDate forKey:NSFileCreationDate];
+						if( ![_fileManager setAttributes:attr ofItemAtPath:fullPath error:nil] )
+						{
+							// cann't set attributes
+							NSLog(@"Failed to set attributes");
+						}
+						
+					}
+					[orgDate release];
+					orgDate = nil;
+				}
+				
+			}
+			
+			if (ret == UNZ_OK) {
+				ret = unzCloseCurrentFile( _unzFile );
+				if (ret != UNZ_OK) {
+					[self OutputErrorMessage:@"file was unzipped but failed crc check"];
+					success = NO;
+				}
+			}
+			
+			if (ret == UNZ_OK) {
+				ret = unzGoToNextFile( _unzFile );
+			}
+			
+			if (_progressBlock && _numFiles) {
+				index++;
+				int p = index*100/_numFiles;
+				progress = p;
+				_progressBlock(progress, index, _numFiles);
+			}
+		}
 	} while (ret==UNZ_OK && ret!=UNZ_END_OF_LIST_OF_FILE);
+	
+#ifdef DEBUG
+	NSLog(@"Unzipping DONE, Skipped %i files", skipped);
+#endif
+	
 	return success;
 }
 
